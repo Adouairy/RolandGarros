47580c1f07efcdbfb87d4548bb0566fff6c63acc
Jeremie Herve <jeremie.herve.jh@gmail.com>
2017-05-17 14:07:53 +0200

    enlerver ce putain de load() '2'

--- projetRolandGarrosJACA/src/main/java/servlet/Accueil.java
+++ projetRolandGarrosJACA/src/main/java/servlet/Accueil.java
@@ -1,19 +1,20 @@
 package servlet;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Accueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
     public Accueil() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
@@ -24,11 +25,4 @@ public class Accueil extends HttpServlet {
 		// TODO Auto-generated method stub
 		this.getServletContext().getRequestDispatcher("/accueil.jsp").forward(request, response);
 	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}
-
 }
