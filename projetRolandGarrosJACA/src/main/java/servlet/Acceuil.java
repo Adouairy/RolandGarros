848272bed0778516991834517b05e13aacd2f542
Jeremie Herve <jeremie.herve.jh@gmail.com>
2017-05-17 14:06:04 +0200

    enlerver ce putain de load()

--- projetRolandGarrosJACA/src/main/java/servlet/Acceuil.java
+++ projetRolandGarrosJACA/src/main/java/servlet/Acceuil.java
@@ -7,17 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AccueilAdmin
- */
-@WebServlet("/AccueilAdmin")
-public class AccueilAdmin extends HttpServlet {
+public class Acceuil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AccueilAdmin() {
+    public Acceuil() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,15 +23,6 @@ public class AccueilAdmin extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.getServletContext().getRequestDispatcher("/accueil.jsp").forward(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
