54114660d9ab60abd79101e0000e966fbc0256dc
AntoineGuillois <guillois.antoine@gmail.com>
2017-05-17 15:16:05 +0200

    avec le exe pour tester

--- projetRolandGarrosJACA/src/main/java/dao/Exe.java
+++ projetRolandGarrosJACA/src/main/java/dao/Exe.java
@@ -5,7 +5,7 @@ import entite.Organisateur;
 public class Exe {
 	public static void main(String[] args) { 		 
 		BaseDAO b = new BaseDAO(); 		
-		Organisateur o = new Organisateur("Garros", true, "jaca"); 	
+		Organisateur o = new Organisateur("Garros", "Roland", "jaca"); 	
 		 
 		System.out.println(b.verifConnection(o) );
 
