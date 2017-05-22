09902ef3983a20126403516c6c2d718c381e23e2
Jeremie Herve <jeremie.herve.jh@gmail.com>
2017-05-22 12:02:43 +0200

    mise à jour Service

--- projetRolandGarrosJACA/src/main/java/metier/ServiceVerifMdp.java
+++ projetRolandGarrosJACA/src/main/java/metier/ServiceVerifMdp.java
@@ -1,10 +1,16 @@
 package metier;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import dao.BaseDAO;
+import entite.Aide;
 
 public class ServiceVerifMdp {
 
@@ -33,6 +39,38 @@ public class ServiceVerifMdp {
 		return verif;
 	}
 
+	public boolean verifMailAide(HttpServletRequest request) {
+		boolean verif = false;
+		String mail = "";
+		mail = request.getParameter("mailAide");
+		List<Aide> listAide = new ArrayList<Aide>();
+		listAide=BaseDAO.getInstance().renvoiAides();
+		
+		for (int i = 0; i < listAide.size(); i++) {
+			if (listAide.get(i).getMail().equals(mail)) {
+				verif = true;
+			}
+		}
+	
+		return verif;
+	}
+	
+	public boolean verifMailAidant(HttpServletRequest request) {
+		boolean verif = false;
+		String mail = "";
+		mail = request.getParameter("mailAide");
+		List<Aide> listAide = new ArrayList<Aide>();
+		listAide=BaseDAO.getInstance().renvoiAidants();
+		
+		for (int i = 0; i < listAide.size(); i++) {
+			if (listAide.get(i).getMail().equals(mail)) {
+				verif = true;
+			}
+		}
+	
+		return verif;
+	}
+	
 	public boolean notifEnvoyerMail(){
 		boolean envoi= false;
 		System.out.println("Simulation de la notification d'email!");
@@ -40,9 +78,15 @@ public class ServiceVerifMdp {
 	}
 
 	public boolean delaiInscription(Date dateInscription){
-		boolean delai=true;
+		boolean delai=false;
 		//faux si le delai est dépassés
-		
+		Calendar cd = new GregorianCalendar(dateInscription.getYear(),dateInscription.getMonth(),dateInscription.getDay()); 
+		cd.add(GregorianCalendar.MONTH, +3); 
+		Date date = cd.getTime();
+		Date now=new Date();
+		if(now.compareTo(date)>0){
+			delai= true;
+		}
 		return delai;
 	}
 }
