495aec0e20a693a127455ba56f37fc0c1fa012c5
Jeremie Herve <jeremie.herve.jh@gmail.com>
2017-05-23 16:46:29 +0200

    modifs Accueil.java --> mettre le mec connecté en session

--- projetRolandGarrosJACA/src/main/java/servlet/Accueil.java
+++ projetRolandGarrosJACA/src/main/java/servlet/Accueil.java
@@ -9,84 +9,102 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.BaseDAO;
 import entite.Aidant;
+import entite.Aide;
 import metier.ServiceVerifMdp;
 
 public class Accueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+
 	BaseDAO dao = BaseDAO.getInstance();
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Accueil() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Accueil() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/* Création ou récupération de la session */
+		HttpSession session = request.getSession();
 		/**
-         * fonction qui teste le temps de presence d'un aidant sans confirmation dans 
-         * la base de donnée
-         */
-        List<Aidant> listeAidant =new ArrayList<Aidant>();
-        listeAidant = BaseDAO.getInstance().renvoiAidants();
-        Date inscription = new Date();
-        boolean delaiDepasse = true;
-        for (int i = 0; i < listeAidant.size(); i++) {
-            inscription = listeAidant.get(i).getDateInscription();
-            if(inscription!=null){
-            delaiDepasse=ServiceVerifMdp.getInstance().delaiInscription(inscription);
-            System.out.println(listeAidant.get(i));
-            }
-            if(!delaiDepasse){
-            	System.out.println(listeAidant.get(i));
-                BaseDAO.getInstance().supprimerAidant(listeAidant.get(i));
-            }
-        }
-        
-        /**
-         * Vérification de la connection
-         */
-        String table= request.getParameter("select");
-		String email= request.getParameter("email");
-		String mdp= request.getParameter("mdpIdentifiant");
-		
+		 * fonction qui teste le temps de presence d'un aidant sans confirmation
+		 * dans la base de donnée
+		 */
+		List<Aidant> listeAidant = new ArrayList<Aidant>();
+		listeAidant = BaseDAO.getInstance().renvoiAidants();
+		Date inscription = new Date();
+		boolean delaiDepasse = true;
+		for (int i = 0; i < listeAidant.size(); i++) {
+			inscription = listeAidant.get(i).getDateInscription();
+			if (inscription != null) {
+				delaiDepasse = ServiceVerifMdp.getInstance().delaiInscription(inscription);
+				System.out.println(listeAidant.get(i));
+			}
+			if (!delaiDepasse) {
+				System.out.println(listeAidant.get(i));
+				BaseDAO.getInstance().supprimerAidant(listeAidant.get(i));
+			}
+		}
+
+		/**
+		 * Vérification de la connection
+		 */
+		String table = request.getParameter("select");
+		String email = request.getParameter("email");
+		String mdp = request.getParameter("mdpIdentifiant");
+
 		/**
 		 * Récupérer l'aidant ayant cet email
 		 */
-		
-		 List<Aidant> listeAidantVerif =new ArrayList<Aidant>();
-	     listeAidantVerif = BaseDAO.getInstance().renvoiAidants();
-	        
-		String message= null;
-		String MessageErreur= null;
-		//booleen
+
+		String message = null;
+		String MessageErreur = null;
+		// booleen
 		System.out.println(email);
-		Boolean estDansLaBase=dao.verifConnection(email,mdp,table);
-//		Boolean estRef = dao.;
-		if (estDansLaBase){
+		Boolean estDansLaBase = dao.verifConnection(email, mdp, table);
+		// Boolean estRef = dao.;
+		if (estDansLaBase) {
 			if (table.equals("aidant")) {
-				this.getServletContext().getRequestDispatcher("/WEB-INF/accueilAdmin.jsp").forward(request, response);
+				List<Aidant> listeAidantVerif = new ArrayList<Aidant>();
+				listeAidantVerif = BaseDAO.getInstance().renvoiAidants();
+				Aidant aidant = ServiceVerifMdp.getInstance().importerAidant(email);
+				session.setAttribute("leConnecte", aidant);
+				if (aidant.getPremiereconnection() == 1) {
+					this.getServletContext().getRequestDispatcher("/WEB-INF/premierConnection.jsp").forward(request,
+							response);
+				} else if (aidant.getReferent()) {
+					this.getServletContext().getRequestDispatcher("/WEB-INF/espaceConnecterAideReferent.jsp")
+							.forward(request, response);
+				} else {
+					this.getServletContext().getRequestDispatcher("/WEB-INF/accueilAdmin.jsp").forward(request,
+							response);
+				}
 			} else if (table.equals("aide")) {
-				this.getServletContext().getRequestDispatcher("/WEB-INF/espaceConnecterAideReferent.jsp").forward(request, response);
-			} else if (table.equals("aidant") /*&& estRef*/) {
-				this.getServletContext().getRequestDispatcher("/WEB-INF/espaceConnecterAideReferent.jsp").forward(request, response);
+				List<Aide> listeAideVerif = new ArrayList<Aide>();
+				listeAideVerif = BaseDAO.getInstance().renvoiAides();
+				Aide aide = ServiceVerifMdp.getInstance().importerAide(email);
+				session.setAttribute("leConnecte", aide);
+				this.getServletContext().getRequestDispatcher("/WEB-INF/espaceConnecterAideReferent.jsp")
+						.forward(request, response);
+			} else {
+				MessageErreur = "Erreur";
+				message = "Connection échouée, veuillez réessayer";
+				request.setAttribute("MessageErreur", MessageErreur);
+				request.setAttribute("message", message);
+				this.getServletContext().getRequestDispatcher("/accueil.jsp").forward(request, response);
 			}
+
 		}
-		else{
-			MessageErreur="Erreur";
-			message = "Connection échouée, veuillez réessayer";
-			request.setAttribute("MessageErreur", MessageErreur);
-			request.setAttribute("message", message);
-			this.getServletContext().getRequestDispatcher("/accueil.jsp").forward(request, response);
-		}
-		
 	}
 }
-
